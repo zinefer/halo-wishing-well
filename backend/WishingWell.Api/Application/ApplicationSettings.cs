28b9ed550e14ddaccf093d9754ee2b157e3a1a4d
zinefer <zinefer@gmail.com>
2021-12-14 02:00:08 -0700

    Style fix

--- backend/WishingWell.Api/Application/ApplicationSettings.cs
+++ backend/WishingWell.Api/Application/ApplicationSettings.cs
@@ -3,6 +3,7 @@ namespace WishingWell.Api.Application
     public class ApplicationSettings
     {
         public string ApplicationName { get; set; }
+        
         public bool DONOTUSE_ONLY_FOR_CI_DISABLE_AUTHENTICATION { get; set; }
     }
 }
